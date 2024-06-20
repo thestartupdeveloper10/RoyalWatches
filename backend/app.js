6172ac526e1b0015ffe1beac90d483fb74725a94
thestartupdeveloper10 <thestartupdeveloper0@gmail.com>
2024-06-20 09:36:52 +0300

    controllers

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+const config = require('./utils/config')
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+const middleware = require('./utils/middleware')
+// const notesRouter = require('./controllers/notes')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+const authRouter = require('./controllers/auth')
+const usersRouter = require('./controllers/users')
+const productRouter = require('./controllers/product')
+const cartRouter =require('./controllers/cart')
+const orderRouter = require('./controllers/order')
+mongoose.set('strictQuery', false)
+
+
+const url = config.MONGODB_URI
+
+
+logger.info('connecting to db')
+
+mongoose.connect(url)
+
+  .then(result => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+// app.use('/api/notes', notesRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/productS', productRouter)
+app.use('/api/carts', cartRouter)
+app.use('/api/orders', orderRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ backend/controllers/auth.js
@@ -0,0 +1,58 @@
+const bcrypt = require('bcrypt')
+const authRouter = require('express').Router()
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+
+authRouter.post('/register', async (request, response) => {
+  const { username, email, password } = request.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    email,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
+})
+
+
+authRouter.post('/login', async (request, response) => {
+  const { username, password } = request.body
+
+  const user = await User.findOne({ username })
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash)
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
+
+  // token expires in 60*60 seconds, that is, in one hour
+  const token = jwt.sign(   
+     userForToken,
+     process.env.SECRET,
+     { expiresIn: 60*60 }  
+    )
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name })
+})
+
+
+
+module.exports = authRouter;
