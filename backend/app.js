9729254a327babaf8ef0eb9f64f960a7c87ebd4a
thestartupdeveloper10 <thestartupdeveloper0@gmail.com>
2024-06-20 12:51:33 +0300

    stripe

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ const usersRouter = require('./controllers/users')
 const productRouter = require('./controllers/product')
 const cartRouter =require('./controllers/cart')
 const orderRouter = require('./controllers/order')
+const stripeRoute = require('./controllers/stripe')
 mongoose.set('strictQuery', false)
 
 
@@ -42,6 +43,7 @@ app.use('/api/users', usersRouter)
 app.use('/api/productS', productRouter)
 app.use('/api/carts', cartRouter)
 app.use('/api/orders', orderRouter)
+app.use("/api/checkout", stripeRoute);
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
