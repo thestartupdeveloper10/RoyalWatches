3cbb8bca3b59a4f12f56b0ae3b80696db59a8d2b
thestartupdeveloper10 <thestartupdeveloper0@gmail.com>
2024-06-23 11:30:03 +0300

    filter

--- frontend/src/component/pages/Product_List.jsx
+++ frontend/src/component/pages/Product_List.jsx
@@ -14,7 +14,52 @@ const Product_List = () => {
           <h1 className="font-extrabold lg:text-[56px] md:text-[36px] text-2xl">Royalwatches Men&apos;s watches</h1>
         </div>
         <div>
-            <div className='lg:mb-20 mb-8'>Category</div>
+            <div className='lg:mb-20 mb-8 bg-[#f3f3a844]'>
+              
+            <div className="flex justify-between flex-wrap">
+        <div className="m-5 flex md:flex  items-center sm:w-auto">
+          <span className="md:text-xl font-semibold mr-5 sm:mr-0">Filter Products:</span>
+          <select
+            name="color"
+            onChange={()=>{}}
+            className="p-2 m-0 mr-3 sm:mr-5 sm:m-5"
+          >
+            <option disabled>Color</option>
+            <option>white</option>
+            <option>black</option>
+            <option>red</option>
+            <option>blue</option>
+            <option>yellow</option>
+            <option>green</option>
+          </select>
+          <select
+            name="size"
+            onChange={()=>{}}
+            className="p-2 m-0 sm:mr-5 sm:m-5"
+          >
+            <option disabled>Size</option>
+            <option>XS</option>
+            <option>S</option>
+            <option>M</option>
+            <option>L</option>
+            <option>XL</option>
+          </select>
+        </div>
+        <div className="m-5 flex items-center sm:flex-row sm:w-auto">
+          <span className="md:text-xl font-semibold mr-5 sm:mr-0">Sort Products:</span>
+          <select
+            onChange={()=>{}}
+            className="p-2 m-0 sm:mr-5 sm:m-5"
+          >
+            <option value="newest">Newest</option>
+            <option value="asc">Price (asc)</option>
+            <option value="desc">Price (desc)</option>
+          </select>
+        </div>
+      </div>
+
+
+            </div>
             <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
 
                 {popularProducts.map((item)=>{
