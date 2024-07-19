a6fd4068d0f661b6fa2144a8c3d2142156376fd6
thestartupdeveloper10 <thestartupdeveloper0@gmail.com>
2024-07-19 16:18:23 +0300

    change api

--- frontend/src/component/Products.jsx
+++ frontend/src/component/Products.jsx
@@ -12,8 +12,8 @@ function Products({ cat, filters, sort, searchTerm }) {
       try {
         const res = await axios.get(
           cat
-            ? `http://localhost:3000/api/products?category=${cat}`
-            : "http://localhost:3000/api/products"
+            ? `https://royalwatches-backend.onrender.com/api/products?category=${cat}`
+            : "https://royalwatches-backend.onrender.com/api/products"
         );
         setProducts(res.data);
       } catch (err) {
